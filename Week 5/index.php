cc8b53902e8b25154a6bb29b39123652fe9278e3
NACHIKETA CHOUDHARY <67115256+nachiketchoudhary@users.noreply.github.com>
2021-05-12 01:40:28 +0530

    Add files via upload

---
+++ Week 5/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Nachiketa Choudhary </title>
+</head>
+<body>
+    
+
+<h1>Nachiketa Choudhary PHP</h1>
+<p>
+    <?php
+	print "The SHA256 hash of \"Nachiketa Choudhary\" is ".hash('sha256', 'Nachiketa Choudhary');
+?>
+</p>
+<pre>ASCII ART:
+
+  _   _ 
+ | \ | |
+ |  \| |
+ | . ` |
+ | |\  |
+ |_| \_|
+        
+        
+
+</pre>
+<a href="fail.php">error setting</a>
+<br>
+<a href="check.php">cause a traceback</a>
+</body>
+</html>
---
+++ Week 6/Week6.php
@@ -0,0 +1,22 @@
+<html>
+<body>
+<title>Nachiketa Choudhary edc3c2ed </title>
+<h1> Welcome to my guessing game</h1>
+
+<?php
+if ( ! isset($_GET['guess']) ) { 
+    echo("Missing guess parameter");
+  } else if ( strlen($_GET['guess']) < 1 ) {
+    echo("Your guess is too short");
+  } else if ( ! is_numeric($_GET['guess']) ) {
+    echo("Your guess is not a number");
+  } else if ( $_GET['guess'] < 65 ) {
+    echo("Your guess is too low");
+  } else if ( $_GET['guess'] > 65) {
+    echo("Your guess is too high");
+  } else {
+    echo("Congratulations - You are right");
+  }
+?>
+</body>
+</html>
